d36e8c2f1b0a7510d860928edee2082ce93afdf4
Gupuroom <cjsrnr1114@gmail.com>
2022-04-13 10:28:44 +0900

    WhereToGo Page API 생성 : All Place 조회하는 API 추가

--- src/main/java/com/delgo/api/domain/Place.java
+++ src/main/java/com/delgo/api/domain/Place.java
@@ -1,17 +1,32 @@
 package com.delgo.api.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
 
-@Entity
 @Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Place {
     @Id
     @Column(name = "place_id")
-    private long id;
+    private long placeId;
     private String name;
     private String address;
+
+    @CreationTimestamp
     @Column(name = "regist_dt")
-    private String registDt;
+    private Timestamp registDt;
+
+    @Column(name = "main_photo_url")
+    private String mainPhotoUrl;
+
+    private String lowestPrice;
 }
---
+++ src/main/java/com/delgo/api/service/PlaceService.java
@@ -0,0 +1,23 @@
+package com.delgo.api.service;
+
+import com.delgo.api.domain.Place;
+import com.delgo.api.repository.PlaceRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PlaceService {
+
+    private final PlaceRepository placeRepository;
+
+    public List<Place> getAllPlace() {
+        return placeRepository.findAll();
+    }
+}
