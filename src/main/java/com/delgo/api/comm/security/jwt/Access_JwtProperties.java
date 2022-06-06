931189022c64ddeb68a99234d22c293b9a15c46f
Gupuroom <cjsrnr1114@gmail.com>
2022-06-07 08:18:06 +0900

    Access_Token 제한 시간 변경 1분 -> 10분

--- src/main/java/com/delgo/api/comm/security/jwt/Access_JwtProperties.java
+++ src/main/java/com/delgo/api/comm/security/jwt/Access_JwtProperties.java
@@ -2,7 +2,7 @@ package com.delgo.api.comm.security.jwt;
 
 public interface Access_JwtProperties {
 	String SECRET = "Access_DelgoSecretKey"; // Key 값
-	int EXPIRATION_TIME = 60000; // 10분
+	int EXPIRATION_TIME = 600000; // 10분
 	String TOKEN_PREFIX = "Access ";
 	String HEADER_STRING = "Authorization_Access";
 }
