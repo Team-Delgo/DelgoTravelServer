99b780195d0627ac15c1b1a1792b3080e27ad63a
Gupuroom <cjsrnr1114@gmail.com>
2022-06-07 11:43:30 +0900

    Common Validation 설정 : DTO Validation Error Method 추가

--- src/main/java/com/delgo/api/comm/exception/ExceptionController.java
+++ src/main/java/com/delgo/api/comm/exception/ExceptionController.java
@@ -3,6 +3,7 @@ package com.delgo.api.comm.exception;
 import com.delgo.api.dto.common.ResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,23 +15,25 @@ public class ExceptionController {
     // 알 수 없는 에러 체크
     @ExceptionHandler
     public ResponseEntity exception(Exception e) {
+        e.printStackTrace();
         return ResponseEntity.ok().body(
                 ResponseDTO.builder().code(303).codeMsg("알 수 없는 오류입니다.").build());
     }
 
-
     // @RequestParam Param Error Check
     @ExceptionHandler({MissingServletRequestParameterException.class})
     public ResponseEntity missingServletRequestParameterException(MissingServletRequestParameterException e) {
-        log.info("Param Type Error" + e.getMessage());
+        log.info("RequestParam PARAM ERROR" + e.getMessage());
         return ResponseEntity.ok().body(
                 ResponseDTO.builder().code(ApiCode.PARAM_ERROR.getCode()).codeMsg(ApiCode.PARAM_ERROR.getMsg()).build());
     }
-//
-//    @ExceptionHandler({SQLException.class})
-//    public ResponseEntity sQLException(SQLException e) {
-//        log.info("Param Error"+ e.getMessage());
-//        return ResponseEntity.ok().body(
-//                ResponseDTO.builder().code(303).codeMsg("DB ERROR").build());
-//    }
+
+    // @RequestBody DTO Param Error Check
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.info("RequestBody DTO PARAM ERROR" + e.getMessage());
+        return ResponseEntity.ok().body(
+                ResponseDTO.builder().code(ApiCode.PARAM_ERROR.getCode()).codeMsg(ApiCode.PARAM_ERROR.getMsg()).build());
+    }
+
 }
