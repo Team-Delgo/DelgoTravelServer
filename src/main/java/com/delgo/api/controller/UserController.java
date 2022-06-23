7c5dd85b22ead26ea88bd40ff5657176a98e30cd
danpung2 <danpung_2@naver.com>
2022-06-23 18:10:16 +0900

    펫 칼럼 수정, UserDTO -> SignUpDTO

--- src/main/java/com/delgo/api/controller/UserController.java
+++ src/main/java/com/delgo/api/controller/UserController.java
@@ -5,17 +5,14 @@ import com.delgo.api.comm.exception.ApiCode;
 import com.delgo.api.domain.pet.Pet;
 import com.delgo.api.domain.user.User;
 import com.delgo.api.dto.InfoDTO;
-import com.delgo.api.dto.UserDTO;
+import com.delgo.api.dto.SignUpDTO;
 import com.delgo.api.comm.security.jwt.Access_JwtProperties;
 import com.delgo.api.comm.security.jwt.Refresh_JwtProperties;
 import com.delgo.api.service.PetService;
 import com.delgo.api.service.TokenService;
 import com.delgo.api.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -44,9 +41,9 @@ public class UserController extends CommController {
 
     // 펫 정보 수정
     @PostMapping("/changePetInfo")
-    public ResponseEntity<?> changePetInfo(@Validated @RequestBody UserDTO userDTO){
+    public ResponseEntity<?> changePetInfo(@Validated @RequestBody SignUpDTO signUpDTO){
         try{
-            String checkedEmail = userDTO.getUser().getEmail();
+            String checkedEmail = signUpDTO.getUser().getEmail();
 
             if(checkedEmail == null){
                 return ErrorReturn(ApiCode.PARAM_ERROR);
@@ -55,7 +52,7 @@ public class UserController extends CommController {
             User user = userService.getUserByEmail(checkedEmail);
             int userId = user.getUserId();
             Pet originPet = petService.getPetByUserId(userId);
-            Pet changePet = userDTO.getPet();
+            Pet changePet = signUpDTO.getPet();
 
             changePet.setUserId(originPet.getUserId());
 
@@ -67,9 +64,6 @@ public class UserController extends CommController {
 
             if(changePet.getSize() != null)
                 originPet.setSize(changePet.getSize());
-
-            if(changePet.getWeight() != 0)
-                originPet.setWeight(changePet.getWeight());
 
             petService.changePetInfo(originPet);
 
@@ -85,10 +79,10 @@ public class UserController extends CommController {
 
     // 비밀번호 수정
     @PostMapping("/changePassword")
-    public ResponseEntity<?> changePassword(@Validated @RequestBody UserDTO userDTO){
+    public ResponseEntity<?> changePassword(@Validated @RequestBody SignUpDTO signUpDTO){
         try {
-            String checkedEmail = userDTO.getUser().getEmail();
-            String newPassword = userDTO.getUser().getPassword();
+            String checkedEmail = signUpDTO.getUser().getEmail();
+            String newPassword = signUpDTO.getUser().getPassword();
 
             if(checkedEmail == null || newPassword == null)
                 return ErrorReturn(ApiCode.PARAM_ERROR);
@@ -207,18 +201,18 @@ public class UserController extends CommController {
 
     // 소셜 회원가입
     @PostMapping("/socialSignup")
-    public ResponseEntity<?> registerUserBySocial(@Validated @RequestBody UserDTO userDTO, HttpServletResponse response) {
+    public ResponseEntity<?> registerUserBySocial(@Validated @RequestBody SignUpDTO signUpDTO, HttpServletResponse response) {
         try {
-            String phoneNoUpdate = userDTO.getUser().getPhoneNo().replaceAll("[^0-9]", "");
-            String birthdayUpdate = userDTO.getPet().getBirthday().replaceAll("[^0-9]", "");
-            userDTO.getUser().setPhoneNo(phoneNoUpdate);
-            userDTO.getPet().setBirthday(birthdayUpdate);
-            userDTO.getUser().setEmail("");
+            String phoneNoUpdate = signUpDTO.getUser().getPhoneNo().replaceAll("[^0-9]", "");
+            String birthdayUpdate = signUpDTO.getPet().getBirthday().replaceAll("[^0-9]", "");
+            signUpDTO.getUser().setPhoneNo(phoneNoUpdate);
+            signUpDTO.getPet().setBirthday(birthdayUpdate);
+            signUpDTO.getUser().setEmail("");
 
-            User user = userService.socialSignup(userDTO.getUser(), userDTO.getPet());
+            User user = userService.socialSignup(signUpDTO.getUser(), signUpDTO.getPet());
 
-            String Access_jwtToken = tokenService.createToken("Access", userDTO.getUser().getPhoneNo()); // Access Token 생성
-            String Refresh_jwtToken = tokenService.createToken("Refresh", userDTO.getUser().getPhoneNo()); // Refresh Token 생성
+            String Access_jwtToken = tokenService.createToken("Access", signUpDTO.getUser().getPhoneNo()); // Access Token 생성
+            String Refresh_jwtToken = tokenService.createToken("Refresh", signUpDTO.getUser().getPhoneNo()); // Refresh Token 생성
 
             response.addHeader(Access_JwtProperties.HEADER_STRING, Access_JwtProperties.TOKEN_PREFIX + Access_jwtToken);
             response.addHeader(Refresh_JwtProperties.HEADER_STRING, Refresh_JwtProperties.TOKEN_PREFIX + Refresh_jwtToken);
@@ -233,17 +227,17 @@ public class UserController extends CommController {
 
     // 회원가입
     @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@Validated @RequestBody UserDTO userDTO, HttpServletResponse response) {
+    public ResponseEntity<?> registerUser(@Validated @RequestBody SignUpDTO signUpDTO, HttpServletResponse response) {
         try {
-            String phoneNoUpdate = userDTO.getUser().getPhoneNo().replaceAll("[^0-9]", "");
-            String birthdayUpdate = userDTO.getPet().getBirthday().replaceAll("[^0-9]", "");
-            userDTO.getUser().setPhoneNo(phoneNoUpdate);
-            userDTO.getPet().setBirthday(birthdayUpdate);
-             User user = userService.signup(userDTO.getUser(), userDTO.getPet());
+            String phoneNoUpdate = signUpDTO.getUser().getPhoneNo().replaceAll("[^0-9]", "");
+            String birthdayUpdate = signUpDTO.getPet().getBirthday().replaceAll("[^0-9]", "");
+            signUpDTO.getUser().setPhoneNo(phoneNoUpdate);
+            signUpDTO.getPet().setBirthday(birthdayUpdate);
+             User user = userService.signup(signUpDTO.getUser(), signUpDTO.getPet());
              user.setPassword(""); // 보안
 
-            String Access_jwtToken = tokenService.createToken("Access", userDTO.getUser().getEmail()); // Access Token 생성
-            String Refresh_jwtToken = tokenService.createToken("Refresh", userDTO.getUser().getEmail()); // Refresh Token 생성
+            String Access_jwtToken = tokenService.createToken("Access", signUpDTO.getUser().getEmail()); // Access Token 생성
+            String Refresh_jwtToken = tokenService.createToken("Refresh", signUpDTO.getUser().getEmail()); // Refresh Token 생성
 
             response.addHeader(Access_JwtProperties.HEADER_STRING, Access_JwtProperties.TOKEN_PREFIX + Access_jwtToken);
             response.addHeader(Refresh_JwtProperties.HEADER_STRING, Refresh_JwtProperties.TOKEN_PREFIX + Refresh_jwtToken);
@@ -258,9 +252,9 @@ public class UserController extends CommController {
 
     // 회원탈퇴
     @PostMapping("/deleteUser")
-    public ResponseEntity<?> deleteUser(@Validated @RequestBody UserDTO userDTO){
+    public ResponseEntity<?> deleteUser(@Validated @RequestBody SignUpDTO signUpDTO){
         try{
-            userService.deleteUser(userDTO.getUser().getEmail());
+            userService.deleteUser(signUpDTO.getUser().getEmail());
 
             return SuccessReturn();
         } catch (NullPointerException e){
