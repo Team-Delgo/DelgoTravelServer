7a3d32a20cccb59bf4dd960ab8e8a82dd2376464
Gupuroom <cjsrnr1114@gmail.com>
2022-06-24 10:38:24 +0900

    Room Service 메서드 추가 : getRoomByRoomId 추가

--- src/main/java/com/delgo/api/repository/RoomRepository.java
+++ src/main/java/com/delgo/api/repository/RoomRepository.java
@@ -4,7 +4,10 @@ import com.delgo.api.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RoomRepository extends JpaRepository<Room, Integer> {
     List<Room> findByPlaceId(int placeId);
+
+    Optional<Room> findByRoomId(int roomId);
 }
