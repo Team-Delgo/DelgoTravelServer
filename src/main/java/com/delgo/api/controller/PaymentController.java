32960932f394014a8cba47924930b2d9f4205d4a
danpung2 <danpung_2@naver.com>
2022-07-05 15:52:22 +0900

    결제 취소 api

--- src/main/java/com/delgo/api/controller/PaymentController.java
+++ src/main/java/com/delgo/api/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package com.delgo.api.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -39,82 +40,32 @@ public class PaymentController {
 
     private final String SECRET_KEY = "test_sk_ADpexMgkW36GJqKEJoBVGbR5ozO0";
 
-    @RequestMapping("/success")
-    public String confirmPayment(
-            @RequestParam String paymentKey, @RequestParam String orderId, @RequestParam Long amount,
-            Model model) throws Exception {
+    @PostMapping("/payment/cancel")
+    public void requestRefund(@RequestBody String cancelReason, @PathVariable String paymentKey){
 
         HttpHeaders headers = new HttpHeaders();
-        // headers.setBasicAuth(SECRET_KEY, ""); // spring framework 5.2 이상 버전에서 지원
         headers.set("Authorization", "Basic " + Base64.getEncoder().encodeToString((SECRET_KEY + ":").getBytes()));
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         Map<String, String> payloadMap = new HashMap<>();
-        payloadMap.put("orderId", orderId);
-        payloadMap.put("amount", String.valueOf(amount));
+        payloadMap.put("cancelReason", cancelReason);
 
-        HttpEntity<String> request = new HttpEntity<>(objectMapper.writeValueAsString(payloadMap), headers);
+        HttpEntity<String> request = null;
+        try {
+            request = new HttpEntity<>(objectMapper.writeValueAsString(payloadMap), headers);
+        } catch (JsonProcessingException e) {
+
+        }
 
         ResponseEntity<JsonNode> responseEntity = restTemplate.postForEntity(
-                "https://api.tosspayments.com/v1/payments/" + paymentKey, request, JsonNode.class);
+                "https://api.tosspayments.com/v1/payments/" + paymentKey + "/cancel", request, JsonNode.class);
 
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
-            JsonNode successNode = responseEntity.getBody();
-            model.addAttribute("orderId", successNode.get("orderId").asText());
-            String secret = successNode.get("secret").asText(); // 가상계좌의 경우 입금 callback 검증을 위해서 secret을 저장하기를 권장함
-            return "success";
+            // 성공
         } else {
-            JsonNode failNode = responseEntity.getBody();
-            model.addAttribute("message", failNode.get("message").asText());
-            model.addAttribute("code", failNode.get("code").asText());
-            return "fail";
+            // 실패
         }
+
     }
 
-    @RequestMapping("/fail")
-    public String failPayment(@RequestParam String message, @RequestParam String code, Model model) {
-        model.addAttribute("message", message);
-        model.addAttribute("code", code);
-        return "fail";
-    }
-
-    @RequestMapping("/virtual-account/callback")
-    @ResponseStatus(HttpStatus.OK)
-    public void handleVirtualAccountCallback(@RequestBody CallbackPayload payload) {
-        if (payload.getStatus().equals("DONE")) {
-            // handle deposit result
-        }
-    }
-
-    private static class CallbackPayload {
-        public CallbackPayload() {}
-
-        private String secret;
-        private String status;
-        private String orderId;
-
-        public String getSecret() {
-            return secret;
-        }
-
-        public void setSecret(String secret) {
-            this.secret = secret;
-        }
-
-        public String getStatus() {
-            return status;
-        }
-
-        public void setStatus(String status) {
-            this.status = status;
-        }
-
-        public String getOrderId() {
-            return orderId;
-        }
-
-        public void setOrderId(String orderId) {
-            this.orderId = orderId;
-        }
-    }
 }
