f585f557631f18397e1281c26b4b38ce650c27b1
Gupuroom <cjsrnr1114@gmail.com>
2022-07-14 15:13:59 +0900

    Place Controller 수정 : recommend 메서드 두개 존재 한개 삭제
    Booking State
    : 주석 수정

--- src/main/java/com/delgo/api/controller/PlaceController.java
+++ src/main/java/com/delgo/api/controller/PlaceController.java
@@ -169,26 +169,6 @@ public class PlaceController extends CommController {
      * Response Data : 검색조건에 부합하는 PlaceList 반환
      */
     @GetMapping("/recommend")
-    public ResponseEntity recommendPlace() {
-        List<Place> placeList = placeService.getPlaceAll();
-        Collections.shuffle(placeList);
-
-        List<Place> returnList = new ArrayList<>();
-        for (int i = 0; i < 5; i++)
-            returnList.add(placeList.get(i));
-
-        return SuccessReturn(returnList);
-    }
-
-    /*
-     * TODO Main 숙소 추천 API ( 보완해야 함 )
-     * Request Data : userId, name, address, startDt, endDt
-     * - userId : wish 여부 check
-     * - name, address : DB에서 일치 여부 조회 (정확히 일치 X 해당 단어 포함 0) [ 빈 값 허용 ]
-     * - startDt, endDt : 예약 가능 여부 및 최저가격 조회에 사용
-     * Response Data : 검색조건에 부합하는 PlaceList 반환
-     */
-    @GetMapping("/recommend")
     public ResponseEntity recommendPlace(@RequestParam Integer userId) {
         List<Place> placeList = placeService.getPlaceAll();
         placeService.setMainPhoto(placeList);
