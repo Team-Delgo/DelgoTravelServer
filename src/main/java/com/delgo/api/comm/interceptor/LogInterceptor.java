f8eca3dd4e5935ff93a572eff395cdcd4aaa2ba1
Gupuroom <cjsrnr1114@gmail.com>
2022-07-14 15:13:05 +0900

    Log 수정 : Post 요청시 RequestBody Param 볼 수 있게 수정

--- src/main/java/com/delgo/api/comm/interceptor/LogInterceptor.java
+++ src/main/java/com/delgo/api/comm/interceptor/LogInterceptor.java
@@ -16,27 +16,31 @@ import java.io.IOException;
 @Slf4j
 public class LogInterceptor implements HandlerInterceptor {
 
+    private final String POST = "POST";
+    private final String TYPE_JSON = "application/json";
+
     // Success Return은 postHandle에서 Log 처리
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView mv) throws Exception {
         ContentCachingResponseWrapper responseWrapper = getResponseWrapper(response);
-        if (responseWrapper != null) {
-            ResponseDTO responseDTO = getResponseBody(responseWrapper);
-
-            if (responseDTO.getCode() == 200)
+        ResponseDTO responseDTO = getResponseBody(responseWrapper);
+        if (responseDTO.getCode() == 200)
+            if (request.getMethod().equals(POST) && request.getContentType().equals(TYPE_JSON))
+                log.info("{} || Result : code = {} msg = {} \n Parameter : {} ", request.getRequestURI(), responseDTO.getCode(), responseDTO.getCodeMsg(),request.getAttribute("requestBody"));
+            else
                 APILog.info(request, responseDTO.getCode(), responseDTO.getCodeMsg());
-        }
     }
 
     // Error Return은 afterCompletion Log 처리
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         ContentCachingResponseWrapper responseWrapper = getResponseWrapper(response);
-        if (responseWrapper != null) {
-            ResponseDTO responseDTO = getResponseBody(responseWrapper);
+        ResponseDTO responseDTO = getResponseBody(responseWrapper);
 
-            // Exception이 발생하면 postHandle을 타지 않는다.
-            if (responseDTO.getCode() != 200) // Service 단 Error Log 처리
+        // Exception이 발생하면 postHandle을 타지 않는다.
+        if (responseDTO.getCode() != 200) // Service 단 Error Log 처리
+            if (request.getMethod().equals(POST) && request.getContentType().equals(TYPE_JSON))
+                log.info("{} || Result : code = {} msg = {} \n Parameter : {} ", request.getRequestURI(), responseDTO.getCode(), responseDTO.getCodeMsg(),request.getAttribute("requestBody"));
+            else
                 ERRLog.info(request, responseDTO.getCode(), responseDTO.getCodeMsg());
-        }
     }
 
 
