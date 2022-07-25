055eb442091718b74b09d55ff73860c61e4c68bc
danpung2 <danpung_2@naver.com>
2022-07-25 13:18:57 +0900

    알림톡 템플릿 변경

--- src/main/java/com/delgo/api/service/AlimService.java
+++ src/main/java/com/delgo/api/service/AlimService.java
@@ -16,14 +16,14 @@ import java.io.IOException;
 public class AlimService {
     private final AlimTalkService alimTalkService;
 
-    public void sendWaitAlimTalk(String templateCode, String phoneNo, String placeName, String roomName, String startDt, String endDt) throws IOException {
+    public void sendWaitAlimTalk(String templateCode, String phoneNo, String placeName, String roomName, String peopleNum, String startDt, String checkIn, String endDt, String checkOut) throws IOException {
         try {
             String content = "[Delgo] 예약대기 안내\n" +
                     "아래 숙소의 예약 대기가 접수 되었습니다.\n" +
                     "숙소이름 : " + placeName + "\n" +
-                    "객실타입 : " + roomName + " (기준인원2명)\n" +
-                    "입실일시: " + startDt + " 15:00 ~\n" +
-                    "퇴실일시: " + endDt + " 11:00\n" +
+                    "객실타입 : " + roomName + " (기준인원 " + peopleNum + " 명)\n" +
+                    "입실일시: " + startDt + " " + checkIn + "~\n" +
+                    "퇴실일시: " + endDt + " "+ checkOut + "\n" +
                     "\n" +
                     "예약 확정 여부는 숙소 확인 후 문자로 발송됩니다.\n" +
                     "(영업시간 이외 접수된 예약대기는 다음날 오전 중 문자로 발송됩니다.\n" +
@@ -35,16 +35,16 @@ public class AlimService {
         }
     }
 
-    public void sendFixAlimTalk(String templateCode, String phoneNo, String userName, String placeName, String roomName, String startDt, String endDt) throws IOException {
+    public void sendFixAlimTalk(String templateCode, String phoneNo, String userName, String placeName, String roomName, String peopleNum, String startDt, String checkIn, String endDt, String checkOut) throws IOException {
         try {
             String content = "[Delgo] 예약확정 안내\n" +
                     "안녕하세요? " + userName +"님의 예약이 확정되었습니다.\n" +
                     "아래 숙소의 예약 정보를 확인해주세요.\n" +
                     "\n" +
                     "숙소이름 : " + placeName + "\n" +
-                    "객실타입 : " + roomName + " (기준인원2명)\n" +
-                    "입실일시: " + startDt + " 15:00 ~\n" +
-                    "퇴실일시: " + endDt + " 11:00\n" +
+                    "객실타입 : " + roomName + " (기준인원 " + peopleNum + " 명)\n" +
+                    "입실일시: " + startDt + " " + checkIn + "~\n" +
+                    "퇴실일시: " + endDt + " "+ checkOut + "\n" +
                     "\n" +
                     "반려견과 좋은 시간 보내세요!";
             alimTalkService.sendAlimTalk(templateCode, phoneNo, content);
@@ -53,14 +53,14 @@ public class AlimService {
         }
     }
 
-    public void sendCancelAlimTalk(String templateCode, String phoneNo, String placeName, String roomName, String price, String payment) throws IOException {
+    public void sendCancelAlimTalk(String templateCode, String phoneNo, String placeName, String roomName, String peopleNum, String price, String payment) throws IOException {
         try {
             String content = "[Delgo] 예약취소 안내\n" +
                     "안녕하세요? Delgo입니다.\n" +
                     "고객님께서 예약하신 숙소가 취소되었습니다.\n" +
                     "\n" +
                     "숙소이름 : " + placeName + "\n" +
-                    "객실타입 : " + roomName + " (기준인원2명)\n" +
+                    "객실타입 : " + roomName + " (기준인원 " + peopleNum + " 명)\n" +
                     "환불예정금액: " + price + "원\n" +
                     "결제수단: " + payment + "\n" +
                     "환불소요기간: 영업일 기준 3~5일\n" +
