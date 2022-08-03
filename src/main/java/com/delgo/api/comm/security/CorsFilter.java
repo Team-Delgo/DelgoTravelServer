e16abaa8a369f7e656672159dd31063be86ded19
Gupuroom <cjsrnr1114@gmail.com>
2022-08-03 15:24:15 +0900

    CORS Filter 수정 dev(개발), real(배포) 구분

--- src/main/java/com/delgo/api/comm/security/CorsFilter.java
+++ src/main/java/com/delgo/api/comm/security/CorsFilter.java
@@ -22,7 +22,8 @@ public class CorsFilter implements Filter {
       HttpServletRequest request = (HttpServletRequest) req;
       HttpServletResponse response = (HttpServletResponse) res;
 
-      response.setHeader("Access-Control-Allow-Origin", "*");
+//      response.setHeader("Access-Control-Allow-Origin", "https://www.delgo.pet"); //TODO: real
+      response.setHeader("Access-Control-Allow-Origin", "*"); // TODO: dev
 //      response.setHeader("Access-Control-Allow-Credentials", "true");
       response.setHeader("Access-Control-Allow-Methods","*");
       response.setHeader("Access-Control-Max-Age", "3600");
