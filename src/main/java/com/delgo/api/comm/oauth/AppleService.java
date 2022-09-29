cddc430e25afd174221e1480e74d0cbb0432f274
Gupuroom <cjsrnr1114@gmail.com>
2022-09-29 13:30:39 +0900

    OAuth Controller : Apple 추가

--- src/main/java/com/delgo/api/comm/oauth/AppleService.java
+++ src/main/java/com/delgo/api/comm/oauth/AppleService.java
@@ -8,24 +8,30 @@ import lombok.NoArgsConstructor;
 import net.minidev.json.JSONObject;
 import org.springframework.stereotype.Service;
 
-@NoArgsConstructor
 @Service
+@NoArgsConstructor
 public class AppleService {
-    public JSONObject decodeFromIdToken(String id_token) {
 
+    // 애플로그인
+//    public static final String TEAM_ID = "GGS2HY6B73";
+//    public static final String REDIRECT_URL = "https://delgo.pet/oauth/redirect/apple";
+//    public static final String CLIENT_ID = "pet.delgo";
+//    public static final String KEY_ID = "P9AZCBNYJG";
+//    public static final String AUTH_URL = "https://appleid.apple.com";
+//    public static final String KEY_PATH = "static/apple/AuthKey_P9AZCBNYJG.p8";
+
+    public JSONObject decodeFromIdToken(String id_token) {
         try {
             SignedJWT signedJWT = SignedJWT.parse(id_token);
-            ReadOnlyJWTClaimsSet getPayload = signedJWT.getJWTClaimsSet();
             ObjectMapper objectMapper = new ObjectMapper();
+
+            ReadOnlyJWTClaimsSet getPayload = signedJWT.getJWTClaimsSet();
             JSONObject payload = objectMapper.readValue(getPayload.toJSONObject().toJSONString(), JSONObject.class);
 
-            if (payload != null) {
-                return payload;
-            }
+            if (payload != null) return payload;
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return null;
     }
 }
