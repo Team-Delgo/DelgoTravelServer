3e6753a57e4acddde1e725b632b66d1605dfb983
Gupuroom <cjsrnr1114@gmail.com>
2022-09-29 11:39:55 +0900

    OAuth Service : Apple 필요없는 코드 삭제

--- src/main/java/com/delgo/api/comm/oauth/AppleService.java
+++ src/main/java/com/delgo/api/comm/oauth/AppleService.java
@@ -1,134 +1,16 @@
 package com.delgo.api.comm.oauth;
 
-import com.delgo.api.service.ECPrivateKeyImpl;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.JWSSigner;
-import com.nimbusds.jose.crypto.ECDSASigner;
-import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.ReadOnlyJWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import lombok.NoArgsConstructor;
 import net.minidev.json.JSONObject;
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
-import org.bouncycastle.util.io.pem.PemObject;
-import org.bouncycastle.util.io.pem.PemReader;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-
-import java.io.FileReader;
-import java.security.interfaces.ECPrivateKey;
-import java.util.*;
 
 @NoArgsConstructor
 @Service
 public class AppleService {
-    private static ObjectMapper objectMapper = new ObjectMapper();
-
-    public String createClientSecret(String teamId, String clientId, String keyId, String keyPath, String authUrl) {
-
-        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.ES256).keyID(keyId).build();
-        JWTClaimsSet claimsSet = new JWTClaimsSet();
-        Date now = new Date();
-
-        claimsSet.setIssuer(teamId);
-        claimsSet.setIssueTime(now);
-        claimsSet.setExpirationTime(new Date(now.getTime() + 3600000));
-        claimsSet.setAudience(authUrl);
-        claimsSet.setSubject(clientId);
-
-        SignedJWT jwt = new SignedJWT(header, claimsSet);
-
-        byte[] privateKey = readPrivateKey(keyPath);
-
-        try {
-            ECPrivateKey ecPrivateKey = new ECPrivateKeyImpl(privateKey);
-            JWSSigner jwsSigner = new ECDSASigner(ecPrivateKey.getS());
-
-            jwt.sign(jwsSigner);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return jwt.serialize();
-    }
-
-
-    private byte[] readPrivateKey(String keyPath) {
-
-        Resource resource = new ClassPathResource(keyPath);
-        byte[] content = null;
-
-        try (FileReader keyReader = new FileReader(resource.getFile());
-             PemReader pemReader = new PemReader(keyReader)) {
-            {
-                PemObject pemObject = pemReader.readPemObject();
-                content = pemObject.getContent();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return content;
-    }
-
-    public String doPost(String url, Map<String, String> param) {
-        String result = null;
-        CloseableHttpClient httpclient = null;
-        CloseableHttpResponse response = null;
-        Integer statusCode = null;
-        String reasonPhrase = null;
-        try {
-            httpclient = HttpClients.createDefault();
-            HttpPost httpPost = new HttpPost(url);
-            httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
-            List<NameValuePair> nvps = new ArrayList<>();
-            Set<Map.Entry<String, String>> entrySet = param.entrySet();
-            for (Map.Entry<String, String> entry : entrySet) {
-                String fieldName = entry.getKey();
-                String fieldValue = entry.getValue();
-                nvps.add(new BasicNameValuePair(fieldName, fieldValue));
-            }
-            UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(nvps);
-            httpPost.setEntity(formEntity);
-            response = httpclient.execute(httpPost);
-            statusCode = response.getStatusLine().getStatusCode();
-            reasonPhrase = response.getStatusLine().getReasonPhrase();
-            HttpEntity entity = response.getEntity();
-            result = EntityUtils.toString(entity, "UTF-8");
-
-            if (statusCode != 200) {
-
-            }
-            EntityUtils.consume(entity);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (response != null) {
-                    response.close();
-                }
-                if (httpclient != null) {
-                    httpclient.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return result;
-    }
-
     public JSONObject decodeFromIdToken(String id_token) {
 
         try {
