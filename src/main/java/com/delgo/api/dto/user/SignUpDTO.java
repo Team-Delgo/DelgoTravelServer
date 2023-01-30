79316ea84b83e0b65f67121c2d8ab860b7dc43d8
Gupuroom <cjsrnr1114@gmail.com>
2023-01-30 09:39:19 +0900

    SignUp test

--- src/main/java/com/delgo/api/dto/user/SignUpDTO.java
+++ src/main/java/com/delgo/api/dto/user/SignUpDTO.java
@@ -1,8 +1,10 @@
 package com.delgo.api.dto.user;
 
+import com.delgo.api.domain.pet.Pet;
 import com.delgo.api.domain.pet.PetSize;
+import com.delgo.api.domain.user.User;
+import com.delgo.api.domain.user.UserSocial;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -10,21 +12,31 @@ import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
-@Builder
 @AllArgsConstructor
 public class SignUpDTO {
-    @NotBlank
-    private String userName;
-    @NotBlank
-    private String email;
-    @NotBlank
-    private String password;
-    @NotBlank
-    private String phoneNo;
-    @NotBlank
-    private String petName;
-    @NotNull
-    private PetSize petSize;
-    @NotNull
-    private LocalDate birthday;
+    @NotBlank private String userName;
+    @NotBlank private String email;
+    @NotBlank private String password;
+    @NotBlank private String phoneNo;
+    @NotBlank private String petName;
+    @NotNull private PetSize petSize;
+    @NotNull private LocalDate birthday;
+
+    public User makeUser(String password){
+        return User.builder()
+                .name(userName)
+                .email(email)
+                .password(password)
+                .phoneNo(phoneNo.replaceAll("[^0-9]", ""))
+                .userSocial(UserSocial.D)
+                .build();
+    }
+
+    public Pet makePet(){
+        return Pet.builder()
+                .name(petName)
+                .size(petSize)
+                .birthday(birthday)
+                .build();
+    }
 }
